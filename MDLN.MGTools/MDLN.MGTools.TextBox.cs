aff377903e1391a267f80b731ef04a6aa11218e3
JBeighel <vodobaas@gmail.com>
2015-09-08 22:45:27 -0400

    Building UI Controls
    Need some UI controls to get information from the user.  Config screens
    or other utilities

---
+++ MDLN.MGTools/MDLN.MGTools.TextBox.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using System;
+
+namespace MDLN.MGTools {
+	public class TextBox : Button, IControl, IVisible {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MDLN.MGTools.TextBox"/> class.
+		/// </summary>
+		/// <param name="GraphDev">Connection to Graph device.</param>
+		/// <param name="Background">Background texture</param>
+		/// <param name="Top">Top coordinate of control</param>
+		/// <param name="Left">Left coordinate of control</param>
+		/// <param name="Height">Height of control</param>
+		/// <param name="Width">Width of control</param>
+		public TextBox(GraphicsDevice GraphDev, Texture2D Background, int Top , int Left, int Height, int Width) : base(GraphDev, Background, Top, Left, Height, Width) {
+			Click += new ClickEvent(ControlClicked);
+		}
+
+		/// <summary>
+		/// True if the control will accept and handle keyboard input, false to ignore keyboard input
+		/// </summary>
+		/// <value><c>true</c> if this instance has focus; otherwise, <c>false</c>.</value>
+		public bool HasFocus { get; set; }
+
+		private void ControlClicked(object Sender, MouseButton Button) {
+			HasFocus = true;
+		}
+	}
+}
+
